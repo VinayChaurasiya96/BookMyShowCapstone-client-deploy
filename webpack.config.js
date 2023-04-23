17239134fbc1ffe0cb4f24acd385904af029310c
Vinay chaurasiya <80849410+VinayChaurasiya96@users.noreply.github.com>
2023-04-24 01:55:17 +0530

    client deploy

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const {EnvironmentPlugin} = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  devServer: {
+    proxy: {
+      "/api/**": {
+        target: "http://localhost:8080/",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new EnvironmentPlugin({
+      API_URI:
+        "https://book-my-show-capstone-server-deploy-2xfwpxxz5-vinaychaurasiya96.vercel.app",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".mjs"],
+  },
+};
